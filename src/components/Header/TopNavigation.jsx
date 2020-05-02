93e71aadbdaff145c20c0e04dd3d78d8c2c9df51
Ldthovn <ledinhtho98bn@gmail.com>
2020-05-02 16:30:28 +0700

    Cl

--- src/components/Header/TopNavigation.jsx
+++ src/components/Header/TopNavigation.jsx
@@ -1,15 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import './TopNavigation.scss';
+import "./TopNavigation.scss";
 
 export default class TopNavigation extends Component {
-  render () {
+  render() {
     return (
       <div className="top_nav">
         <div className="container">
           <div className="row">
             <div className="col-md-6">
-              <div className="top_nav_left">free shipping on all u.s orders over $50</div>
+              <div className="top_nav_left">
+                free shipping on all u.s orders over $50
+              </div>
             </div>
             <div className="col-md-6 text-right">
               <div className="top_nav_right">
@@ -20,10 +22,18 @@ export default class TopNavigation extends Component {
                       <i className="fa fa-angle-down"></i>
                     </a>
                     <ul className="language_selection">
-                      <li><a href="#">French</a></li>
-                      <li><a href="#">Italian</a></li>
-                      <li><a href="#">German</a></li>
-                      <li><a href="#">Spanish</a></li>
+                      <li>
+                        <a href="#">French</a>
+                      </li>
+                      <li>
+                        <a href="#">Italian</a>
+                      </li>
+                      <li>
+                        <a href="#">German</a>
+                      </li>
+                      <li>
+                        <a href="#">Spanish</a>
+                      </li>
                     </ul>
                   </li>
                   <li className="account">
@@ -32,8 +42,18 @@ export default class TopNavigation extends Component {
                       <i className="fa fa-angle-down"></i>
                     </a>
                     <ul className="account_selection">
-                      <li><a href="#"><i className="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
-                      <li><a href="#"><i className="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-sign-in" aria-hidden="true"></i>
+                          Sign In
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-user-plus" aria-hidden="true"></i>
+                          Register
+                        </a>
+                      </li>
                     </ul>
                   </li>
                 </ul>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
-import TopNavigation from './TopNavigation';
-import MainNavigation from './MainNavigation';
+import React, { Component } from "react";
+import TopNavigation from "./TopNavigation";
+import MainNavigation from "./MainNavigation";
 
-import './index.scss';
+import "./index.scss";
 
 export default class Header extends Component {
-  render () {
+  render() {
     return (
       <header className="header trans_300">
         <TopNavigation />
