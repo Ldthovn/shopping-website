69913a52dd114009a921565529bd2a213d85c377
Ldthovn <ledinhtho98bn@gmail.com>
2020-05-02 09:28:04 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import CategoryPage from "./containers/CategoryPage/CategoryPage";
+
+class App extends Component {
+  render() {
+    return <CategoryPage />;
+  }
+}
+
+export default App;
